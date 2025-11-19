99e1df27b3a8fc24556759247eb7b7ba58e36b60
Guilherme M. Miranda <alchemist.software@proton.me>
2025-11-19 10:31:17 -0300

    alright let's do this

--- src/p_udmf.c
+++ src/p_udmf.c
@@ -46,30 +46,39 @@
 
 typedef enum
 {
-    NO_FEATURES   = (0), // shut compiler up, also reset when parsing new map
+    NO_FEATURES = (0), // shut compiler up, also reset when parsing new map
 
     THING_FRIEND  = (1u << 1), // Marine's Best Friend :)
     THING_SPECIAL = (1u << 2), // Death/Pickup/etc-activated actions
     THING_PARAM   = (1u << 3), // ditto, also customizes some MObjs
-    THING_ALPHA   = (1u << 4), // opacity percentage
-    THING_TRANMAP = (1u << 5), // ditto, also customizable LUT
+    THING_HEALTH  = (1u << 4), // WIP
+    THING_ALPHA   = (1u << 5), // opacity percentage
+    THING_TRANMAP = (1u << 6), // ditto, also customizable LUT
+    THING_TINT    = (1u << 7), // WIP
 
-    LINE_PARAM    = (1u << 6), // Hexen-style param actions
-    LINE_PASSUSE  = (1u << 7), // Boom's "Pass Use Through" line flag
-    LINE_BLOCK    = (1u << 8), // MBF21's entity blocking flag
-    LINE_3DMIDTEX = (1u << 9), // EE's 3D middle texture
-    LINE_ALPHA    = (1u << 10), // opacity percentage
-    LINE_TRANMAP  = (1u << 11), // ditto, also customizable LUT
+    LINE_PARAM    = (1u << 8), // Hexen-style param actions
+    LINE_PASSUSE  = (1u << 9), // Boom's "Pass Use Through" line flag
+    LINE_BLOCK    = (1u << 10), // MBF21's entity blocking flag
+    LINE_3DMIDTEX = (1u << 12), // EE's 3D middle texture
+    LINE_ALPHA    = (1u << 13), // opacity percentage
+    LINE_TRANMAP  = (1u << 14), // ditto, also customizable LUT
+    LINE_TINT     = (1u << 15), // WIP
 
-    SIDE_OFFSET   = (1u << 12), // texture X/Y alignment
-    SIDE_SCROLL   = (1u << 13), // texture scrolling property
-    SIDE_LIGHT    = (1u << 14), // independent light levels
+    SIDE_OFFSET  = (1u << 16), // texture X/Y alignment
+    SIDE_SCROLL  = (1u << 17), // texture scrolling property
+    SIDE_LIGHT   = (1u << 18), // independent light levels
+    SIDE_ALPHA   = (1u << 19), // WIP
+    SIDE_TRANMAP = (1u << 20), // WIP
+    SIDE_TINT    = (1u << 21), // WIP
 
-    SEC_ANGLE     = (1u << 15), // plane rotation
-    SEC_OFFSET    = (1u << 16), // plane X/Y alignment
-    SEC_EE_SCROLL = (1u << 17), // EE's original plane scrolling property
-    SEC_SCROLL    = (1u << 18), // DSDA's later plane scrolling property
-    SEC_LIGHT     = (1u << 19), // independent light levels
+    SEC_ANGLE     = (1u << 22), // plane rotation
+    SEC_OFFSET    = (1u << 23), // plane X/Y alignment
+    SEC_EE_SCROLL = (1u << 24), // EE's original plane scrolling property
+    SEC_SCROLL    = (1u << 25), // DSDA's later plane scrolling property
+    SEC_LIGHT     = (1u << 26), // independent light levels
+    SEC_COLORMAP  = (1u << 27), // WIP
+    SEC_TINT      = (1u << 28), // WIP
+    SEC_SILENCE   = (1u << 29), // WIP
 
     // Compatibility
     COMP_NO_ARG0 = (1u << 31),
